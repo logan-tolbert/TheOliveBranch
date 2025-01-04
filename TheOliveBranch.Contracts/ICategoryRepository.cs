d2b776c7a02a78493e578cb05bfea501f5b15ba9
logan-Tolbert <lodev205@gmail.com>
2025-01-04 12:16:16 -0600

    :bricks: Add category repository and interface
    - Add ICategoryRepository interface with methods for updating and saving categories.
    - Add CategoryRepository class and implemented the ICategory interface

---
+++ TheOliveBranch.Contracts/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using TheOliveBranch.Models;
+
+namespace TheOliveBranch.Contracts
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
