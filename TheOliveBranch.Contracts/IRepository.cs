3df8f7a43814bf31b6b52155d8657d05f2d913bf
logan-Tolbert <lodev205@gmail.com>
2025-01-03 18:45:09 -0600

    :construction: Add class libraries, class, and interface
    - Initial setup for repository pattern
    - Add new class libraries `TheOliveBranch.Contracts` and
    `TheOliveBranch.Repo`
    - Create `IRepository<T>` generic
    interface for data access methods
    - Implement `Repository<T>` class using Entity Framework Core.

---
+++ TheOliveBranch.Contracts/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace TheOliveBranch.Contracts
+{
+    public interface IRepository<T> where T : class 
+    {
+        void Add(T entity);
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+        IEnumerable<T> GetAll();
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);  
+    }
+}
